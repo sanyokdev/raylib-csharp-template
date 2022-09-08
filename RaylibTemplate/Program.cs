cc4528e88c4137ad1a455381783b3ee9721ddafc
Alessandro Nikolaev <alessandronikolaev.business@gmail.com>
2022-09-08 23:24:14 +0100

    Initial full project commit

---
+++ RaylibTemplate/Program.cs
@@ -0,0 +1,23 @@
+﻿using Raylib_cs;
+
+namespace RaylibTemplate;
+
+internal static class Program
+{
+    public static void Main()
+    {
+        Raylib.InitWindow(640, 360, "Hello World");
+        
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing(); 
+            Raylib.ClearBackground(Color.WHITE);
+            
+            Raylib.DrawText("Hello, world!", 16, 16, 20, Color.BLACK);
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
